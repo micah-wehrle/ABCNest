3b31f8261db7610fbf5915a934396af6c0af85bf
micah.wehrle <micah23@gmail.com>
2023-04-24 13:17:38 -0400

    Combined :uuid/ and :uuid/:amount routes

--- src/jobs/controllers/jobs.controller.ts
+++ src/jobs/controllers/jobs.controller.ts
@@ -4,30 +4,9 @@ import { TechLoad } from '../models/tech-load.model';
 
 @Controller('/jobs')
 export class JobsController {
-  @Get('/get/:uuid')
-  async getJobsGeneral(@Param('uuid') uuid: string) {
-    // await new Promise(resolve => setTimeout(resolve, 1500)); // just adds a delay for feel
-
-    if (!uuid || uuid.length === 0) {
-      return {
-        'flowStatus': 'FAILURE',
-        'flowStatusMessage': 'invalid UUID',
-      }
-    }
-
-    const jobData = new TechLoad(uuid);
-
-
-    return {
-      'flowStatus': 'SUCCESS',
-      'flowStatusMessage': 'IDK success',
-      'jobData': jobData,
-    }
-  }
-
-  @Get('/get/:uuid/:amount')
+  @Get(['get/:uuid', '/get/:uuid/:amount'])
   async getJobsSpecific(@Param('uuid') uuid: string, @Param('amount') amount: string) {
-    // await new Promise(resolve => setTimeout(resolve, 1500)); // just adds a delay for feel
+    await new Promise(resolve => setTimeout(resolve, 1500)); // just adds a delay for feel
 
     if (!uuid || uuid.length === 0) {
       return {
@@ -36,27 +15,12 @@ export class JobsController {
       }
     }
 
-    const amountNum: number = Number.parseInt(amount);
-
-    if (!amountNum || typeof amountNum !== 'number' || amountNum <= 0) {
-      return {
-        'flowStatus': 'FAILURE',
-        'flowStatusMessage': 'invalid job amount request',
-      }
-    }
-
-    const jobData: TechLoad = new TechLoad(uuid, amountNum);
-
+    const jobData: TechLoad = new TechLoad(uuid, Number.parseInt(amount));
 
     return {
       'flowStatus': 'SUCCESS',
       'flowStatusMessage': 'IDK success',
       'jobData': jobData,
     }
-  }
- 
-  @Post('/update/:id')
-  updateJob(@Body() body: any, @Param('id') id: any) {
-    
   }
 }
