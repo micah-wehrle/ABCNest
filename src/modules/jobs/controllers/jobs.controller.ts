3327d1a5d6ca80c6b89c425ec9c6015a84013880
micah.wehrle <micah23@gmail.com>
2023-04-24 22:30:55 -0400

    Tweaked job controller response

--- src/modules/jobs/controllers/jobs.controller.ts
+++ src/modules/jobs/controllers/jobs.controller.ts
@@ -7,7 +7,7 @@ export class JobsController {
     await new Promise(resolve => setTimeout(resolve, 1000));
     return {
       'flowStatus': 'SUCCESS',
-      'flowStatusMessage': 'IDK success',
+      'flowStatusMessage': 'Successfully transmitted UUID.',
       'data': uuid,
     }
   }
