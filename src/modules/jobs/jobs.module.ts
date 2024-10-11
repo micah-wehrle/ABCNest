d867da1bf4cfb5efa1f17b173a3c8c211a21748a
Brandon Kounse <kounse@gmail.com>
2024-10-10 20:47:20 -0500

    added controller and added controller to module for routing

--- src/modules/jobs/jobs.module.ts
+++ src/modules/jobs/jobs.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { JobsController } from './controllers/jobs.controller';
+import { PokemonController } from './controllers/pokemon.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  controllers: [JobsController]
+  controllers: [JobsController, PokemonController],
+  imports: [HttpModule]
 })
 export class JobsModule {}
